029a9de698f950489763965a40ed7b925bbadc6c
golden-state0fmind <areyes2009@yahoo.com>
2022-03-07 11:58:39 -0700

    adds rest api routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //imports
 const express = require('express');
 const morgan = require('morgan');
-//const routes = require('./routes'); This will point to the routes folder 
+const routes = require('./routes');
 
 //sets up server
 const port = process.env.PORT || 4000;
@@ -12,11 +12,17 @@ app.use(morgan('dev'));//server logging
 app.use(express.json()); //JSON parsing
 
 //API routes 
-// app.use('/api/v1/', routes);
-// app.use('/api/v1/', roues);
-// app.use('/api/v1/', routes);
+app.use('/api/v1/user', routes.user);
+app.use('/api/v1/groupticket', routes.groupticket);
+app.use('/api/v1/individualticket', routes.individualticket);
+app.use('/api/v1/location', routes.location);
+app.use('/api/v1/item', routes.item);
+app.use('/api/v1/friend', routes.friend);
+app.use('/api/v1/login', routes.login);
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Bill Buddy!')
 });
 
 //connection
