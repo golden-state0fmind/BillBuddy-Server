60805d6338af8d48c6b99cb13c1c6a12f960dba2
golden-state0fmind <areyes2009@yahoo.com>
2022-03-07 11:56:37 -0700

    changes name of locationId column for reference

--- migrations/20220201030451-create-location.js
+++ migrations/20220201030451-create-location.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      locationID: {
+      locationId: {
         type: Sequelize.INTEGER
       },
       localTaxes: {
